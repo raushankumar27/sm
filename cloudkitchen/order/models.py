4bc4229b8433773211af918fa427268fa5b6060e
raushan kumar pandey <raushan.kumar478@gmail.com>
2017-05-21 18:28:20 +0530

    created some models

--- cloudkitchen/order/models.py
+++ cloudkitchen/order/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class products(models.Model):
+    product_id=models.AutoField(primary_key=True)
+    product_name=models.CharField(max_length=200)
+    price=models.IntegerField()
+
+class order(models.Model):
+    orderno=models.AutoField(primary_key=True)
+    product_id=models.ForeignKey(products)
+    #and some more
+
+class Addons(models.Model):
+    Product_name=models.CharField(max_length=200)
+    Main_product=models.ForeignKey(products)
